d4813887b9642a9c6ae6c4c8931a57cf7a1b8e7d
zxlie <xianliezhao@foxmail.com>
2016-07-04 13:29:44 +0800

    移除.idea目录

--- build.sh
+++ build.sh
@@ -2,17 +2,15 @@
 MOD_NAME="JsTracker"
 
 #把需要的文件都copy到相应的目录下
-rm -rf output && mkdir output
-cp -r static output/
-cp -r _locales output/
-cp online.manifest.json output/manifest.json
-
+rm -rf output && mkdir -p output/${MOD_NAME}
+cp -r static output/${MOD_NAME}/
+cp manifest.json output/${MOD_NAME}/
 
 # 冗余文件清理
 cd output/
 
 #生成zip包
-zip -r $MOD_NAME.zip $MOD_NAME/ > /dev/null
+zip -r $MOD_NAME.zip $MOD_NAME/
 
 echo ""
 echo "生成压缩包成功，可发布到Chrome web store了！"
